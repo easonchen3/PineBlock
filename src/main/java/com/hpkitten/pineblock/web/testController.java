25b770142d46da552707eac9319b93e04dbf66d7
xubaojian <1263697416@qq.com>
2019-05-16 18:30:38 +0800

    test

--- src/main/java/com/hpkitten/pineblock/web/testController.java
+++ src/main/java/com/hpkitten/pineblock/web/testController.java
@@ -1,7 +1,8 @@
 package com.hpkitten.pineblock.web;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
@@ -10,14 +11,14 @@ import java.util.Map;
  * @Date: 2019-05-16 17:13
  * @Version 1.0
  */
-@Controller
+@RestController
 public class testController {
 
 
     @RequestMapping("home")
-    public String goHome(Map<String , Object> paramMap){
+    public ResponseEntity<Map> goHome(Map<String , Object> paramMap){
         paramMap.put("name","zhangshan");
         paramMap.put("age",35);
-        return "/home";
+        return ResponseEntity.ok(paramMap);
     }
 }
